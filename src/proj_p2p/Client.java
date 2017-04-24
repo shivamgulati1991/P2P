bcdc4cc6d21e869152515b4f5968cd043898c090
shivam gulati <shivamgulati1991@gmail.com>
2017-04-24 02:29:29 -0400

    Setting peers

--- src/proj_p2p/Client.java
+++ src/proj_p2p/Client.java
@@ -1,10 +1,74 @@
 package proj_p2p;
 
-public class Client {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
+public class Client implements Runnable {
+	private static final String version = "P2P-CI/1.0";
+	public static List<Rfc> rfcList=Collections.synchronizedList(new ArrayList<Rfc>());
+	public ServerSocket serverSocket;
+	
+	public Client(int portNo) throws IOException{
+		serverSocket=new ServerSocket(portNo);
+		System.out.println("Starting client now..");
+		System.out.println("Client is at Host: "+InetAddress.getLocalHost().getHostAddress());
+		System.out.println("Port number: "+serverSocket.getLocalPort());
+		new Thread(this).start();
+	}
+	
+	public static int getRandomPort(int min, int max) {
+		Random random = new Random();
+		int randomPort = random.nextInt((max - min) + 1) + min;
+	    return randomPort;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String IPaddr=null,hostName = null;
+		if(args.length==2){
+			IPaddr=args[0];
+			int port=Integer.parseInt(args[1]);
+			try {
+				hostName=InetAddress.getByName(IPaddr).getHostName();
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			Socket clientSocket=null;
+			ObjectOutputStream output=null;
+			ObjectInputStream input=null;
+			int randomPort=Client.getRandomPort(3000, 5000);
+			try{
+				clientSocket=new Socket(IPaddr,port);
+				output=new ObjectOutputStream(clientSocket.getOutputStream());
+				input=new ObjectInputStream(clientSocket.getInputStream());
+				output.writeObject(hostName.toString());
+				int clientPort=clientSocket.getLocalPort();
+				
+				System.out.println("Client is running now."+"Randomport: "+randomPort);
+				System.out.println("Hostname: "+hostName+"  Port: "+clientPort);
+			}
+			catch(Exception e){
+				System.err.print(e);
+			}
+		}
+		else{
+			System.out.println("Incorrect arguments entered.");
+		}
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- src/proj_p2p/Server.java
+++ src/proj_p2p/Server.java
@@ -6,17 +6,21 @@ import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Server implements Runnable {
 	private static final String version = "P2P-CI/1.0";
-	public static LinkedList<Rfc> rfcList=new LinkedList<Rfc>();
-	public static LinkedList<Peers> peerList=new LinkedList<Peers>();
-	public ServerSocket socket;
+	public static List<Rfc> rfcList=Collections.synchronizedList(new ArrayList<Rfc>());
+	public static List<Peers> peerList=Collections.synchronizedList(new ArrayList<Peers>());
+	public ServerSocket sock;
 	
 	public Server(int portNo) throws IOException{
-		socket=new ServerSocket(portNo);
-      	System.out.println("Host: "+ InetAddress.getLocalHost().getHostAddress()+ "  Port: " + socket.getLocalPort());
+		sock=new ServerSocket(portNo);
+		System.out.println("Starting server now..");
+      	System.out.println("Host: "+ InetAddress.getLocalHost().getHostAddress()+ "  Port: " + sock.getLocalPort());
 		System.out.println("OS: " + System.getProperty("os.name"));
 		System.out.println("Version: " + version);
 		new Thread(this).start();
@@ -52,11 +56,27 @@ public class Server implements Runnable {
 	public void run() {
 		// TODO Auto-generated method stub
 		
-		Socket sock=null;
-		ObjectInputStream  inputstream = null; 
-	    ObjectOutputStream outputstream = null;
-	    String host = null;
+		Socket socket=null;
+		ObjectInputStream  input;
+	    ObjectOutputStream output;
+	    String hostName = null;
 	    int cPort = 0;
+	    try{
+		    socket = sock.accept();
+	        new Thread(this).start();
+	        cPort = socket.getPort();
+			System.out.println("Client connected at port: "+ cPort);
+			input = new ObjectInputStream (socket.getInputStream());
+		    output = new ObjectOutputStream(socket.getOutputStream());
+		    hostName = (input.readObject()).toString();
+			
+		    //List peerObj = new peerList(hostName, Integer.toString(cPort));
+			//peerList.add(peerObj);
+			//System.out.println("Peer Added successfully");
+	    }
+	    catch(Exception e){
+	    	System.err.println(e);
+	    }
 		
 	}
 
